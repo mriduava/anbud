23232859a468a29fad929f19be59a15d70fad61f
mriduava <mriduava@gmail.com>
2020-12-01 21:24:36 +0100

    initial java-spring setup

---
+++ src/main/java/com/mriduava/anbud/AnbudApplication.java
@@ -0,0 +1,13 @@
+package com.mriduava.anbud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnbudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnbudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mriduava/anbud/AnbudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mriduava.anbud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnbudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
