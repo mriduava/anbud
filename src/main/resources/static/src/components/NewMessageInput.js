36f326be3f27114418a7ce2e10dd865a43b1085d
mriduava <mriduava@gmail.com>
2020-12-12 09:36:03 +0100

    add functions in frontend to send message via socket

--- src/main/resources/static/src/components/NewMessageInput.js
+++ src/main/resources/static/src/components/NewMessageInput.js
@@ -1,34 +1,42 @@
+import { sendMessage } from '../socket.js'
+
 export default {
-  template: `
-    <form @submit.prevent="newMessage">
-        <input v-model="messageText" placeholder="type new message..">
-        <button>SEND</button>
-    </form>
-  `,
-  data() {
-    return {
-      messageSender: 'Anon',
-      messageText: ''
-    }
-  },
-  methods: {
-    async newMessage() {
-      let message = {
-          sender: 'Mriduava',
-          content: this.messageText,
-          timestamp: Date.now()
-      }
+    template: `
+        <form @submit.prevent="newMessage">
+            <input v-model="username" style="width: 80px" placeholder="sender">
+            <input v-model="messageText" placeholder="type new message..">
+            <button>SUBMIT</button>
+        </form>
+    `,
+    data() {
+        return {
+            messageSender: 'Anon',
+            messageText: ''
+        }
+    },
+    computed: {
+        username: {
+            get() {
+                return this.$store.state.user && this.$store.state.user.name || this.messageSender
+            },
+            set(val) {
+                this.messageSender = val
+            }
+        }
+    },
+    methods: {
+        async newMessage() {
+            let message = {
+                sender: this.username,
+                content: this.messageText,
+                // timestamp: Date.now()
+            }
 
-      this.messageText = ''
+            // clear input
+            this.messageText = ''
 
-      let res = await fetch('/rest/messages', {
-          method: 'POST',
-          headers: { 'Content-Type': 'application/json'},
-          body: JSON.stringify(message)
-      })
-      // res = await res.json()
-      // this.$store.commit('prependMessage', res)
-        
+            // send message with websocket
+            sendMessage(message)
+        }
     }
-  }
 }
