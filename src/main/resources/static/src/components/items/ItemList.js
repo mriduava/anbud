d2de95d3f1e57dbc05080bebd319492423f52952
mriduava <mriduava@gmail.com>
2021-07-06 23:30:34 +0200

    add owner with items & disp items in card

--- src/main/resources/static/src/components/items/ItemList.js
+++ src/main/resources/static/src/components/items/ItemList.js
@@ -1,21 +1,32 @@
- import AuctionItem from './AuctionItem.js'
-
- export default{
-  components:{
-    AuctionItem
-  },
+export default {
   template:`
-    <div>
-      <AuctionItem
-        v-for="item of items" 
-        :items = "item"
-        :key="item.id"
-      />
+  <div class="container">
+    <div class="row mt-4">
+      <div class="col-lg-4" v-for="(item, index) in items" :key="index">
+        <div class="card shadow">
+          <img :src="item.item_image" class="card-img-top" alt="mriduava@gmail.com" style={{height:250px]}>
+          <div class="card-body">
+            <h5 class="card-title">{{item.item_name}}</h5>
+          </div>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item">Price: {{item.initial_price}} SEK</li>
+            <li class="list-group-item">Start: {{start_time}}</li>
+            <li class="list-group-item">End: {{end_time}}</li>
+          </ul>
+        </div>
+      </div>
     </div>
+  </div>
   `,
   computed:{
     items(){
       return this.$store.state.auctions
+    },
+    start_time(){
+      return new Date(this.items.start_date).toLocaleString()
+    },
+    end_time(){
+      return new Date(this.items.stop_date).toLocaleString()
     }
   }
 }
